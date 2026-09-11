7d6ad67f5b8b9646f8dcf990d4b1f2715b49cc91
Stoil Karastoyanov <stoil.karastoyanov@a1.bg>
2026-09-11 09:08:22 +0300

    Add initial project scaffold with Activity entity and EF Core setup

---
+++ Persistence/Migrations/20260910123107_FixCategoryTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategoryTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Categoty",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Categoty");
+        }
+    }
+}
